0ab534a01d79fff6ce4a3512c6c9b72adeb6de3b
Krisha1011 <131949590+Krisha1011@users.noreply.github.com>
2026-03-12 08:42:58 +0530

    Add files via upload

---
+++ FSD-2(B5)/CH-1/1.js
@@ -0,0 +1,3 @@
+const data = require("./j1.json");
+console.log(data.name);
+console.log(data.age);
---
+++ FSD-2(B5)/CH-2/Filesystemmodule.js
@@ -0,0 +1,6 @@
+var ps = require("fs");
+
+//ps=ps.mkdirSync("node"); //make a directory named node
+
+ps.writeFileSync("node/hello.txt","Hello World"); //create a file named hello.txt and write Hello World in it
+
